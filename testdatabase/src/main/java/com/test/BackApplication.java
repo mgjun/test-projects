27a09b376d5a36b3a0f14b635160f153c615d802
mj3018 <mengjun3018@126.com>
2017-07-12 21:38:25 +0800

    第一次上传，搭建项目目录

---
+++ testdatabase/src/main/java/com/test/BackApplication.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * com.test
+ * Author MENGJUN
+ * on 2017/7/11.
+ */
+@SpringBootApplication
+public class BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackApplication.class);
+    }
+}
